c2dc1ad121872263a6e7139fe3d057905771070f
Andrew Devlin and Patrick McGreevy <patrick@>
2017-01-18 11:01:46 -0800

    Add support for translating single word starting with lowercase vowel

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,35 @@
 // business logic
+var vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+
+var translateWord = function(word) {
+  var result = '';
+  var onset = word.charAt(0);
+  var beginsWithVowel = false;
+
+  for (var i = 0; i < vowels.length; i++) {
+    if (onset === vowels[i]) {
+      beginsWithVowel = true;
+      break;
+    }
+  }
+
+  if (beginsWithVowel) {
+    result = word + 'ay';
+  } else {
+    result = word;
+  }
+
+  return result;
+};
 
 // user interface logic
 $(document).ready(function() {
-  $("form#translator").submit(function() {
+  $("form#translator").submit(function(event) {
     event.preventDefault();
     var input = $("#english").val();
+    var output = translateWord(input);
 
-    $("#pig-latin p").text(input);
+    $("#pig-latin p").text(output);
     $("#pig-latin").show();
   });
 });
