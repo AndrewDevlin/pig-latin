a36bb1e1f1cc52439fc3162f374d37a64f71a2c1
Andrew Devlin and Patrick McGreevy <andrew@>
2017-01-18 11:33:01 -0800

    Add support for translating single word starting with consonant(s)

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,45 @@
 // business logic
 var vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
 
+var isVowel = function(letter) {
+  if (letter.length !== 1) {
+    throw new Error("argument must be single-letter string");
+  }
+  for (var i = 0; i < vowels.length; i++) {
+    if (letter === vowels[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+var onsetConsonants = function(word) {
+  var result = '';
+
+  for (var i = 0; i < word.length; i++) {
+    var char = word.charAt(i);
+    if (isVowel(char)) {
+      break;
+    }
+    result += char;
+  }
+
+  return result;
+}
+
 var translateWord = function(word) {
   var result = '';
   var onset = word.charAt(0);
   var beginsWithVowel = false;
 
-  for (var i = 0; i < vowels.length; i++) {
-    if (onset === vowels[i]) {
-      beginsWithVowel = true;
-      break;
-    }
-  }
+  // Check for presence of initial vowel and set flag accordingly
+  beginsWithVowel = isVowel(onset);
 
   if (beginsWithVowel) {
     result = word + 'ay';
   } else {
-    result = word;
+    onset = onsetConsonants(word);
+    result = word.slice(onset.length) + onset + 'ay';
   }
 
   return result;
